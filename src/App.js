0a5c3355bb91213c93fa89507b16b8981a090230
Yash <guptayash134@gmail.com>
2020-09-10 22:59:36 +0530

    Amplify configuration

--- src/App.js
+++ src/App.js
@@ -1,12 +1,16 @@
 import React from 'react';
 import logo from './logo.svg';
 import './App.css';
+import {
+  withAuthenticator,
+  AmplifySignOut
+} from '@aws-amplify/ui-react'
 
 function App() {
   return ( <
     div className = "App" >
     <
-    header className = "App-header" >
+    header >
     <
     img src = {
       logo
@@ -14,18 +18,12 @@ function App() {
     className = "App-logo"
     alt = "logo" / >
     <
-    p >
-    Welcome to learning tutorial of AWS <
-    /p> <
-    a className = "App-link"
-    href = "https://reactjs.org"
-    target = "_blank"
-    rel = "noopener noreferrer" >
-    Learn React <
-    /a> <
-    /header> <
+    h1 > We now have Auth! < /h1> < /
+    header > <
+    AmplifySignOut / >
+    <
     /div>
   );
 }
 
-export default App;
+export default withAuthenticator(App);
